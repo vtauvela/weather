0e0f5c7a4b83d88eb9c36792ce6c86acf2c39664
vtauvela <vtauvela40@engr-134-03.eastern.ewu.edu>
2015-03-02 14:28:43 -0800

    Version 2

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// REF: http://foundation.zurb.com/docs/
+// REF: http://simpleweatherjs.com.
+
+var cheney = '99004';
+var spokane = '99220';
+
+	// 2. _simpleWeather()_ object
+	$.simpleWeather({
+		
+
+		
+	})
