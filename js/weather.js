e73980b096e90897e7ed77b1a180015ed72d3269
vtauvela <vtauvela@gmail.com>
2015-03-17 18:25:00 -0700

    Version 4

--- js/weather.js
+++ js/weather.js
@@ -1,18 +1,19 @@
 // Docs at http://simpleweatherjs.com
 $(document).ready(function() {
   $.simpleWeather({
-  zipcode: '76309',
-  unit: 'f',
-  success: function(weather) {
-    html = '<h2>'+weather.city+', '+weather.region+'</h2>';
-    html += '<img style="float:left;" width="125px" src="images/weather/'+weather.code+'.png">';
-    html += '<p>'+weather.temp+'&deg; '+weather.units.temp+'<br /><span>'+weather.currently+'</span></p>';
-    html += '<a href="'+weather.link+'">View Forecast &raquo;</a>';
- 
-    $("#weather").html(html);
-  },
-  error: function(error) {
-    $("#weather").html("<p>"+error+"</p>");
-  }
-});
+    location: 'Austin, TX',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
 });
